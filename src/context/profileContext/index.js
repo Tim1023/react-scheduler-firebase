879d90e145fb4d00c4d734d2d983f7be024ecb6d
Tim <timzdeveloper23@gmail.com>
2018-03-30 19:46:42 +1300

    Context API -WIP

---
+++ src/context/profileContext/index.js
@@ -0,0 +1,9 @@
+import { createContext } from 'react'
+
+const ProfileContext = createContext({})
+
+export const ProfileProvider = ProfileContext.Provider
+
+export const ProfilerConsumer = ProfileContext.Consumer
+
+//TODO
